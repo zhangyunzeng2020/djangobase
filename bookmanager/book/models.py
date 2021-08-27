5697c097f4eda1d4110ae62442be09de9bcdd2ea
zhangyunzeng2020 <zhangyunzeng2002@163.com>
2021-08-27 15:19:14 +0800

    models.py编写

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+""""
+1、继承自models.Model
+2、自定义主键--id
+3、字段
+    字段名 = model.类型(选项)
+"""
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+
+# 准备人物列表信息的模型类
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键约束：人物属于哪本书
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
